b5c6c912780ef306338b8a07b8f1160d76c827c0
Siddharth Singh <siddharth.singh190902@gmail.com>
2023-07-10 19:29:25 +0530

    inital commit

---
+++ seeds/index.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+
+
+
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 40; i++) {
+        const random1000 = Math.floor(Math.random() * 20);
+        const price = Math.floor(Math.random() * 700) + 40;
+        const camp = new Campground({
+            
+            author: '6490460a06a91bc2939a2858',
+            location: `${cities[i].city}, ${cities[i].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'A very nice area must visit at an affordable price',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[i].longitude,
+                    cities[i].latitude,
+                ]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                },
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                }
+            ]
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
---
+++ seeds/tempcities.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        city: "Manali",
+        title: "Patalsu Peak",
+        latitude: 32.293220,
+        longitude: 77.192470,
+        state: "Himachal Pradesh",
+    },
+    {
+        city: "Manali",
+        title: "Solang Valley",
+        latitude: 32.3099413,
+        longitude: 77.1754183,
+        state: "Himachal Pradesh",
+    },
+    {
+        city: "Leh",
+        title: "Khardungla Pass",
+        latitude: 34.2848302,
+        longitude: 77.7792021,
+        state: "Ladakh",
+    },
+    {
+        city: "Zanskar",
+        title: "Zanskar Valley",
+        latitude: 33.5625314,
+        longitude: 76.9614019,
+        state: "Ladakh",
+    },
+    {
+        city: "Udaipur",
+        title: "Lake Pichola",
+        latitude: 24.5716195,
+        longitude: 73.6823457,
+        state: "Rajasthan",
+    },
+   
+];
